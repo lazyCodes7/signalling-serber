c434a1d7fff5e32f4207ee86467652c6f5093f09
Rishab Mudliar <rishabmudliar@gmail.com>
2023-03-25 12:36:52 +0530

    Update: code for signalling server

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
-from aiohttp import web
-import socketio
 
-ROOM = 'room'
+from flask import Flask, request
+from flask_socketio import SocketIO, emit, join_room
 
-sio = socketio.AsyncServer(cors_allowed_origins='*', ping_timeout=35)
-app = web.Application()
-sio.attach(app)
-
-
-@sio.event
-async def connect(sid, environ):
-    print('Connected', sid)
-    await sio.emit('ready', room=ROOM, skip_sid=sid)
-    sio.enter_room(sid, ROOM)
+app = Flask(__name__)
+app.secret_key = 'random secret key!'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-@sio.event
-def disconnect(sid):
-    sio.leave_room(sid, ROOM)
-    print('Disconnected', sid)
+@socketio.on('join')
+def join(message):
+    username = message['username']
+    room = message['room']
+    join_room(room)
+    print('RoomEvent: {} has joined the room {}\n'.format(username, room))
+    emit('ready', {username: username}, to=room, skip_sid=request.sid)
 
 
-@sio.event
-async def data(sid, data):
-    print('Message from {}: {}'.format(sid, data))
-    await sio.emit('data', data, room=ROOM, skip_sid=sid)
+@socketio.on('data')
+def transfer_data(message):
+    username = message['username']
+    room = message['room']
+    data = message['data']
+    print('DataEvent: {} has sent the data:\n {}\n'.format(username, data))
+    emit('data', data, to=room, skip_sid=request.sid)
+
+
+@socketio.on_error_default
+def default_error_handler(e):
+    print("Error: {}".format(e))
+    socketio.stop()
 
 
 if __name__ == '__main__':
-    web.run_app(app, port=9999)
+    socketio.run(app, host="0.0.0.0", port=5003)
